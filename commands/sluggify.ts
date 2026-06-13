623a6d70352057ada605a28f54392a885474cdf0
PerryRylance <contact@perryrylance.dev>
2026-06-13 07:07:59 +0100

    Use three-slug

---
+++ commands/sluggify.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+import * as THREE from 'three';
+import { SlugLoader, SlugGeometry, injectSlug } from 'three-slug';
+
+// 1. Load the pre-compiled .sluggish binary font data
+new SlugLoader().load('path/to/font.sluggish', (slugData) => {
+
+    
+
+});
